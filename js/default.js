294b670fb30a1654bd5ce4d52afbe97cdc0ae89c
MakLu <imaklu@gmail.com>
2012-02-13 17:52:55 +0800

    static pages ready.

---
+++ js/default.js
@@ -0,0 +1,18 @@
+$(function(){
+    
+    $('.create-task').click(function(){
+        $('#createTask').modal('show');
+        return false;
+    });
+    
+    $('.create-project').click(function(){
+        $('#createProject').modal('show');
+        return false;
+    });
+    
+    $('.create-list').click(function(){
+        $('#createList').modal('show');
+        return false;
+    });
+
+})
